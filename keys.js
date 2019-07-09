a667b4857261b39727e0422023c25baa9b3606ed
pstambaugh14 <pstambaugh14@gmail.com>
2019-07-08 23:51:27 -0400

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,4 +1,4 @@
-// This file will get overwritten upon first run of playbook.yml
+// This file will get overwritten upon first run of playbook.yml.  It resides in mern-auth-ansible/client
 module.exports = {
   mongoURI: "YOUR_MONGO_URI_HERE",
   secretOrKey: "secret"
