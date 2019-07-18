35cc9f95b82388850cdaa818ef972270cda61bee
pstambaugh14 <pstambaugh14@gmail.com>
2019-07-17 20:54:02 -0400

    Added comments
    Added comments to instruct user to configure file with correct URI.
    Once changes have been made, this directory should be automatically mapped as a volume for the container to look for changes (then pushes changes into the running container - in this case, a URI added to a config file).

--- keys.js
+++ keys.js
@@ -1,4 +1,5 @@
-// This file will get overwritten upon first run of playbook.yml.  It resides in mern-auth-ansible/client
+// This file will be mapped locally - meaning that when you configure the correct URI in this file, 
+// it will update inside of the running container also. 
 module.exports = {
   mongoURI: "YOUR_MONGO_URI_HERE",
   secretOrKey: "secret"
