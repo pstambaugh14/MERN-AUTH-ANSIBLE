34c28fbae26fdc1640d523e2b125f696afd3b279
pstambaugh14 <pstambaugh14@gmail.com>
2019-07-17 21:06:01 -0400

    Delete keys.js

--- keys.js
+++
@@ -1,6 +0,0 @@
-// This file will be mapped locally - meaning that when you configure the correct URI in this file, 
-// it will update inside of the running container also. 
-module.exports = {
-  mongoURI: "YOUR_MONGO_URI_HERE",
-  secretOrKey: "secret"
-};
