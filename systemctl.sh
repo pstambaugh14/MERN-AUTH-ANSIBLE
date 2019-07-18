b3838650b38e8362fc2a7f2d428a9c77d76c826e
pstambaugh14 <pstambaugh14@gmail.com>
2019-07-17 21:07:29 -0400

    Delete systemctl.sh

--- systemctl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /opt/test/MERN-AUTH-ANSIBLE && pm2 startup > /opt/test/MERN-AUTH-ANSIBLE/pm2-startup.out
-awk 'NR==3' /opt/test/MERN-AUTH-ANSIBLE/pm2-startup.out
-awkvar=`awk 'NR==3' /opt/test/MERN-AUTH-ANSIBLE/pm2-startup.out`
-echo "$awkvar" > /opt/test/MERN-AUTH-ANSIBLE/awkvar
-chmod +x awkvar
